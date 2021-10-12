425e7341721d294e1a04709725e0581d08e31b2b
Crowdbotics <team@crowdbotics.com>
2021-10-12 13:58:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "sparkling_salad_31346",
+  url: "https://sparkling_salad_31346.botics.co",
+  api: "https://sparkling_salad_31346.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
